0dbfd6bf69a4a24ca1d10ce6bc0a7bd51ec53c7c
jkamess <skkim3@uci.edu>
2021-12-06 00:12:19 -0800

    redid threads

--- src/assignment16a.java
+++ src/assignment16a.java
@@ -2,24 +2,44 @@ import java.util.Random;
 
 class Thread1 extends Thread{
     public void run(){
-       assignment16a.num = assignment16a.rand.nextInt(100);
-        System.out.println("random number is: " + assignment16a.num);
+        while(true){
+            assignment16a.num = assignment16a.rand.nextInt(100);
+            System.out.println("random number is: " + assignment16a.num);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
 
 class Thread2 extends Thread{
     public void run(){
-       if(assignment16a.num%2 == 0){
-           System.out.println("thread 2: "+assignment16a.num*assignment16a.num);
-       }
+        while(true){
+            if(assignment16a.num%2 == 0){
+                System.out.println("thread 2: "+assignment16a.num*assignment16a.num);
+            }
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
 
 class Thread3 extends Thread{
     public void run(){
-        if(assignment16a.num%2 != 0)
-            System.out.println("thread 3: "+assignment16a.num*assignment16a.num*assignment16a.num);
-
+        while(true){
+            if(assignment16a.num%2 != 0)
+                System.out.println("thread 3: "+assignment16a.num*assignment16a.num*assignment16a.num);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
 
@@ -33,9 +53,6 @@ public class assignment16a{
         Thread3 thread3 = new Thread3();
 
         thread.start();
-        while(thread.isAlive()){
-            System.out.println("Waiting...");
-        }
         thread2.start();
         thread3.start();
 
